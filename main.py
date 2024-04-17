5683ef2daa6b027c2105e92af3b6c3189d137a34
Akash NP <akashnp1998@gmail.com>
2024-04-17 23:12:48 +0530

    feat: setup api server via FastAPI

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import requests
 from typing import List
 from decouple import config
 from ytmusicapi import YTMusic
+from fastapi import FastAPI
 
 from models import UserLikedTracks
 
 ytmusic = YTMusic("oauth.json")
 CLIENT_ID = config("CLIENT_ID")
 CLIENT_SECRET = config("CLIENT_SECRET")
+
+app = FastAPI()
 
 def get_access_token() -> str:
     headers = {
@@ -21,7 +24,7 @@ def get_access_token() -> str:
         access_token = data.get("access_token", "")
         return access_token
 
-def get_user_liked_tracks(playlist_id: str) -> List[UserLikedTracks]:
+def get_playlist_songs(playlist_id: str) -> List[UserLikedTracks]:
     headers = { "Authorization": f"Bearer {config('ACCESS_TOKEN')}" }
     response = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}", headers=headers)
     if response.ok:
@@ -39,4 +42,30 @@ def like_songs(songs: List[UserLikedTracks]) -> None:
             print(song_id, song_id)
         # ytmusic.rate_song(song_id, "LIKE")
 
-print(like_songs(get_user_liked_tracks("6ITWYIfjMptEQMhmUp5Wsk")))
+print(like_songs(get_playlist_songs("6ITWYIfjMptEQMhmUp5Wsk")))
+
+@app.get("/spotify/playlists/{playlist_id}/songs")
+def get_spotify_playlist_songs(playlist_id: str):
+    songs = get_playlist_songs(playlist_id)
+    return songs
+
+@app.post("/youtube-music/search")
+def search_youtube_music(payload: List[UserLikedTracks]):
+    result = []
+    for song in payload:
+        yt_result = ytmusic.search(f"{song.name} - {song.artist}", "songs")
+        title = yt_result[0]["title"]
+        artists = ",".join([artist["name"] for artist in yt_result[0]["artists"]])
+        if (song.name in title or title in song.name) and (song.artist in artists or artists in song.artist):
+            match = 1
+        else:
+            match = 0
+        data = {
+            "videoId": yt_result[0]["videoId"],
+            "category": yt_result[0]["category"],
+            "title": title,
+            "artist": artists,
+            "match": match
+        }
+        result.append(data)
+    return result
